79d6dc9f29dd57a6b64563df3c435f6197b8c4fa
Dmitriy Marmyshev <marmyshev@gmail.com>
2016-10-23 23:19:07 +0300

    Added component resolver
    - extention to execute designer

---
+++ com.marmyshev.dt.designer.executor/src/com/marmyshev/dt/designer/executor/AdvancedFileRuntimeComponentResolver.java
@@ -0,0 +1,34 @@
+package com.marmyshev.dt.designer.executor;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import com._1c.g5.v8.dt.platform.services.core.runtimes.execution.ILaunchableRuntimeComponent;
+import com._1c.g5.v8.dt.platform.services.core.runtimes.execution.IRuntimeComponent;
+import com._1c.g5.v8.dt.platform.services.core.runtimes.execution.impl.AbstractFileRuntimeComponentResolver;
+import com._1c.g5.v8.dt.platform.services.model.RuntimeInstallation;
+
+public class AdvancedFileRuntimeComponentResolver extends AbstractFileRuntimeComponentResolver {
+
+	public AdvancedFileRuntimeComponentResolver() {
+		super();
+	}
+
+	@Override
+	public Collection<IRuntimeComponent> resolveComponents(RuntimeInstallation installation) {
+
+		Collection<IRuntimeComponent> result = new ArrayList<IRuntimeComponent>();
+
+		if (installation.getInstallLocation() == null) {
+			return result;
+		}
+
+		ILaunchableRuntimeComponent thickClient = resolveLaunchable(installation, "1cv8", //$NON-NLS-1$
+				"com._1c.g5.v8.dt.platform.services.core.componentTypes.AdvancedThickClient"); //$NON-NLS-1$
+		if (thickClient != null) {
+			result.add(thickClient);
+		}
+		return result;
+	}
+
+}
