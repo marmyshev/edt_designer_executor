b8a1191c4e0f62cb8ca912825aea3f67f018f0be
Dmitriy Marmyshev <marmyshev@gmail.com>
2016-11-06 15:14:58 +0300

    Action and action provider

---
+++ com.marmyshev.dt.designer.executor.ui/src/com/marmyshev/dt/designer/executor/ui/InfobaseDesignerAction.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright (C) 2016, 1C
+ */
+package com.marmyshev.dt.designer.executor.ui;
+
+import org.eclipse.jface.action.Action;
+
+/**
+ * @author marmyshev_d
+ *
+ */
+public class InfobaseDesignerAction
+    extends Action
+{
+    public InfobaseDesignerAction()
+    {
+        super();
+    }
+
+}
